a2aa0a35835fb7ef42444cc3f78259ec19e7a1e3
Andrea Puccio <andrea.puccio@futuryng.com>
2019-03-25 17:49:32 +0100

    updated rollup config file

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,20 @@
 export default {
     input: 'dist/main.js',
-    dest: 'dist/bundles/NgDraggableWidget.umd.js',
-    sourceMap: false,
-    format: 'umd',
-    name: 'ng.draggable.widget',
-    globals: {
-        '@angular/core': 'ng.core',
-        'rxjs/Observable': 'Rx',
-        'rxjs/ReplaySubject': 'Rx',
-        'rxjs/add/operator/map': 'Rx.Observable.prototype',
-        'rxjs/add/operator/mergeMap': 'Rx.Observable.prototype',
-        'rxjs/add/observable/fromEvent': 'Rx.Observable',
-        'rxjs/add/observable/of': 'Rx.Observable'
-    }
+    external: [ '@angular/core' ],
+    output: {
+        file: 'dist/bundles/NgDraggableWidget.umd.js',
+        format: 'cjs',
+        sourceMap: false,
+        format: 'umd',
+        name: 'ng.draggable.widget',
+        globals: {
+            '@angular/core': 'ng.core',
+            'rxjs/Observable': 'Rx',
+            'rxjs/ReplaySubject': 'Rx',
+            'rxjs/add/operator/map': 'Rx.Observable.prototype',
+            'rxjs/add/operator/mergeMap': 'Rx.Observable.prototype',
+            'rxjs/add/observable/fromEvent': 'Rx.Observable',
+            'rxjs/add/observable/of': 'Rx.Observable'
+        }
+    },
 }
